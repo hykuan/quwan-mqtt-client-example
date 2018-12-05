b199edc71e50d0f4eb77de2eb66816f3fd3e7603
hykuan <hykuan1126@gmail.com>
2018-12-05 16:56:02 +0800

    feat(Senml): message should be senml format

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/hykuan/quwan-mqtt-client-example
 
 require (
-	github.com/eclipse/paho.mqtt.golang v1.1.1 // indirect
+	github.com/cisco/senml v0.0.0-20181031221301-910a55054e16
+	github.com/eclipse/paho.mqtt.golang v1.1.1
+	github.com/ugorji/go/codec v0.0.0-20181127175209-856da096dbdf // indirect
 	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/cisco/senml"
 	"github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -49,6 +51,14 @@ func loadConfig() config {
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
+	raw, err := senml.Decode(msg.Payload(), senml.JSON)
+	if err != nil {
+		fmt.Println("got error: ", err)
+		return
+	}
+
+	normalizedData := senml.Normalize(raw)
+	fmt.Println("Normalized data: ", normalizedData)
 }
 
 func main() {
@@ -78,9 +88,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	for i := 0; i < 5; i++ {
-		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish(config.topic, 0, false, text)
+	for i := 0; i < 1; i++ {
+		// the message should be senml format, the following json string is example from mainflux
+		// [{"bn":"some-base-name:","bt":1.276020076001e+09, "bu":"A","bver":5, "n":"voltage","u":"V","v":125}, {"n":"current","t":-5,"v":1.2}, {"n":"current","t":-4,"v":0.382131}]
+		record := []senml.SenMLRecord{
+			{
+				BaseName: "Device_1",
+				BaseTime: float64(time.Now().UnixNano()),
+				Name:     "voltage",
+				Unit:     "V",
+				Value:    func(value float64) *float64 { return &value }(110),
+			},
+			{
+				BaseName: "Device_1",
+				BaseTime: float64(time.Now().UnixNano()),
+				Name:     "temperature",
+				Unit:     "celsius",
+				Value:    func(value float64) *float64 { return &value }(60),
+			},
+		}
+
+		text, _ := json.Marshal(record)
+
+		token := c.Publish(config.topic, 0, false, string(text))
 		token.Wait()
 		time.Sleep(1 * time.Second)
 	}
